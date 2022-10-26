6dce8cb88473ef7a34ef9e32dcb22adb67fa7d9a
benjaminbaffy <benjaminbaffy@gmail.com>
2022-10-26 11:58:06 +0300

    intial training script and module structure change

---
+++ train.py
@@ -0,0 +1,64 @@
+import hydra
+import torch
+import wandb
+from omegaconf import DictConfig
+
+from model import ae, transformer, vae, vqvae
+from util import util
+
+
+def train_step(model, batch):
+    print("afdlkjafdkj")
+
+
+def train(model, dataloader, cfg: DictConfig):
+    for ep in range(cfg.epochs):
+        train_step(model, ep)
+
+
+@hydra.main(version_base=None, config_path="cfg", config_name="train")
+def main(cfg: DictConfig):
+    """The main entry point to the training loop
+
+    Args:
+        cfg (DictConfig): The config object provided by Hydra
+
+    Raises:
+        ValueError: if misconfiguration
+    """
+    if cfg.wandb:
+        wandb.init(
+            project="ccreaim",
+            entity="ccreaim",
+            name=f"{cfg.model}-{cfg.exp_name}-{str(cfg.seed)}-{str(cfg.run_id)}",
+            group=f"{cfg.model}-{cfg.exp_name}",
+            config=cfg,
+        )
+
+    util.set_seed(cfg.seed)
+
+    if cfg.model == "ae":
+        model = None
+    elif cfg.model == "vae":
+        model = None
+    elif cfg.model == "vq-vae":
+        model = None
+    elif cfg.model == "transformer":
+        model = None
+    elif cfg.model == "end-to-end":
+        model = None
+    else:
+        raise ValueError(f"Model type{cfg.model} is not defined!")
+
+    if cfg.model != "transformer":
+        # get sound dataset for training
+        data = None
+    else:
+        # get feature dataset for training
+        data = None
+
+    train(model, data, cfg)
+
+
+if __name__ == "__main__":
+    main()
