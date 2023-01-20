5a2821294d518ed0f80e485f20d46ec0e66daa2e
benjaminbaffy <benjaminbaffy@gmail.com>
2023-01-20 11:33:07 +0200

    test 8bit changes

--- notebooks/test_8bit.py
+++ notebooks/test_8bit.py
@@ -8,7 +8,7 @@ from omegaconf import OmegaConf
 from torch.nn import functional as F
 
 from ccreaim.model import operate
-from ccreaim.utils import audio_tools, cfg_classes, dataset
+from ccreaim.utils import audio_tools, cfg_classes, dataset, util
 
 data_tar = "/scratch/other/sopi/CCREAIM/datasets/test/out/8bit_test.tar"
 load_transformer_path = "/scratch/other/sopi/CCREAIM/logs/2023-01-07/8bit-transformer_train_01-29-58/3/checkpoints/transformer_seqlen-1600_bs-32_lr-0.0001_seed-0_ep-004.pt"
@@ -24,7 +24,7 @@ get_model = operate.get_model_init_function(hyper_cfg)
 model = get_model()
 model.load_state_dict(model_state_dict)
 model = model.to(device)
-
+model = model.eval()
 
 tmp_data_root = dataset.prepare_dataset_on_tmp(data_tar=data_tar)
 data = dataset.Audio8BitDataset(tmp_data_root, hyper_cfg.seq_len)
@@ -56,17 +56,37 @@ torchaudio.save(
 
 
 with torch.inference_mode():
-    seq = sample.to(device)
-    src = F.one_hot(seq.long(), num_classes=256).int()
+    src_batch = F.one_hot(sample.long(), num_classes=256).int()
+    src = F.one_hot(sample_cat.long(), num_classes=256).int()
+    src_batch = src_batch.to(device)
     src = src.to(device)
-    gen = model.generate_chunks(src, 160, 160, 160)
+
+    gen_size = 1600
+    gen = model.generate_chunks(src, gen_size)
     gen = gen.argmax(dim=-1)
+    gen = gen[:, gen_size:]
+
+    src_batch_tgt_mask = util.get_tgt_mask(src_batch.size(1))
+    src_batch_tgt_mask = src_batch_tgt_mask.to(device)
+    inf = model(src_batch, tgt_mask=src_batch_tgt_mask, no_cache=True)
+    inf = inf.argmax(dim=-1)
 
 gen_cat = gen.view(-1).unsqueeze(0)
 print(gen_cat.size())
 torchaudio.save(
     "/scratch/other/sopi/CCREAIM/datasets/test/out/8bit_test_generation.wav",
     gen_cat.cpu().to(torch.uint8),
+    16000,
+    encoding="PCM_U",
+    bits_per_sample=8,
+    format="wav",
+)
+
+inf_cat = inf.view(-1).unsqueeze(0)
+print(inf_cat.size())
+torchaudio.save(
+    "/scratch/other/sopi/CCREAIM/datasets/test/out/8bit_test_reconstruction.wav",
+    inf_cat.cpu().to(torch.uint8),
     16000,
     encoding="PCM_U",
     bits_per_sample=8,
